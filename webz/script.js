598e0d3981a62ba476c3d046169e7c1c9db26261
1ypi <137641825+1ypi@users.noreply.github.com>
2023-06-25 15:23:00 +0200

    Add files via upload

---
+++ webz/script.js
@@ -0,0 +1,10 @@
+    document.addEventListener("DOMContentLoaded", function() {
+        var title = document.title;
+        var count = 0;
+        var variations = ["1", "1y", "1yp", "1ypi"];
+        var interval = setInterval(function() {
+        document.title = variations[count % variations.length];
+        count++;
+        }, 500);
+    });
+    
