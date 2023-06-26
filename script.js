644a2985a8abdbb89a5603a50f0b3c487b1b07b3
1ypi <137641825+1ypi@users.noreply.github.com>
2023-06-26 12:52:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-    document.addEventListener("DOMContentLoaded", function() {
-        var title = document.title;
-        var count = 0;
-        var variations = ["1", "1y", "1yp", "1ypi"];
-        var interval = setInterval(function() {
-        document.title = variations[count % variations.length];
-        count++;
-        }, 500);
-    });
-    
+document.addEventListener("DOMContentLoaded", function() {
+  const video = document.getElementById("backgroundVideo");
+  const clickText = document.getElementById("clickText");
+  const audioPlayer = document.getElementById("audioPlayer");
+
+  // Play the video on page load
+  video.play();
+
+  // Show the click text
+  clickText.style.opacity = "1";
+
+  // Handle click event
+  function handleClick() {
+    audioPlayer.play(); // Play the audio
+    video.style.opacity = "0"; // Fade out the video
+    clickText.classList.add("clicked"); // Add class to fade out the click text
+
+    // Remove the click event listener after it's clicked
+    clickText.removeEventListener("click", handleClick);
+  }
+
+  // Add click event listener to the click text
+  clickText.addEventListener("click", handleClick);
+});
