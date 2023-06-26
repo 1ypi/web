9fde40f1b16496e1a4f28dd4fc7bdb2a9c543a32
1ypi <137641825+1ypi@users.noreply.github.com>
2023-06-26 13:22:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ document.addEventListener("DOMContentLoaded", function() {
   const clickText = document.getElementById("clickText");
   const audioPlayer = document.getElementById("audioPlayer");
 
-  // Play the video on page load
-  video.play();
-
   // Show the click text
   clickText.style.opacity = "1";
 
@@ -15,18 +12,19 @@ document.addEventListener("DOMContentLoaded", function() {
     video.style.opacity = "0"; // Fade out the video
     clickText.style.opacity = "0"; // Fade out the click text
 
-    // Set the new video source
-    video.src = "https://cdn.discordapp.com/attachments/1122169102941814865/1122554551732543618/vecteezy_animated-flying-through-the-stars-and-blue-and-red-in-space_8079146_731.mp4"; // Reemplaza "URL_DEL_NUEVO_VIDEO" con la URL del nuevo video
-
-    video.load(); // Load the new video
-    video.play(); // Play the new video
-
-    clickText.classList.add("clicked"); // Add class to fade out the click text
-
     // Remove the click event listener after it's clicked
     clickText.removeEventListener("click", handleClick);
   }
 
   // Add click event listener to the click text
   clickText.addEventListener("click", handleClick);
+
+  // Pause the video before changing the source
+  video.pause();
+
+  // Set the new video source
+  video.src = "https://cdn.discordapp.com/attachments/1122169102941814865/1122554551732543618/vecteezy_animated-flying-through-the-stars-and-blue-and-red-in-space_8079146_731.mp4";
+
+  // Play the video after the source is changed
+  video.play();
 });
