9cf44761568e4a37b9141914d4820a16804d8315
1ypi <137641825+1ypi@users.noreply.github.com>
2023-06-26 13:10:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,13 @@ document.addEventListener("DOMContentLoaded", function() {
   // Handle click event
   function handleClick() {
     audioPlayer.play(); // Play the audio
-    video.style.opacity = "0"; // Fade out the video
+
+    // Set the new video source
+    video.src = "https://cdn.discordapp.com/attachments/1122169102941814865/1122554551732543618/vecteezy_animated-flying-through-the-stars-and-blue-and-red-in-space_8079146_731.mp4"; // Reemplaza "URL_DEL_NUEVO_VIDEO" con la URL del nuevo video
+
+    video.load(); // Load the new video
+    video.play(); // Play the new video
+
     clickText.classList.add("clicked"); // Add class to fade out the click text
 
     // Remove the click event listener after it's clicked
