f01a7bc11d1bfa4f4fe9c4ebc6a5303eb14314cd
1ypi <137641825+1ypi@users.noreply.github.com>
2023-06-26 13:29:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,26 +3,27 @@ document.addEventListener("DOMContentLoaded", function() {
   const clickText = document.getElementById("clickText");
   const audioPlayer = document.getElementById("audioPlayer");
 
+  // Play the video on page load
+  video.play();
+
   // Show the click text
   clickText.style.opacity = "1";
 
   // Handle click event
   function handleClick() {
     audioPlayer.play(); // Play the audio
-    video.style.opacity = "0"; // Fade out the video
-    clickText.style.opacity = "0"; // Fade out the click text
+    clickText.style.opacity = "0";
+
+    // Set the new video source
+    video.src = "https://cdn.discordapp.com/attachments/1122169102941814865/1122554551732543618/vecteezy_animated-flying-through-the-stars-and-blue-and-red-in-space_8079146_731.mp4"; // Reemplaza "URL_DEL_NUEVO_VIDEO" con la URL del nuevo video
+
+    video.load(); // Load the new video
+    video.play(); // Play the new video
+
+    clickText.classList.add("clicked"); // Add class to fade out the click text
 
     // Remove the click event listener after it's clicked
     clickText.removeEventListener("click", handleClick);
-
-    // Set the new video source
-    video.src = "https://cdn.discordapp.com/attachments/1122169102941814865/1122554551732543618/vecteezy_animated-flying-through-the-stars-and-blue-and-red-in-space_8079146_731.mp4";
-
-    // Wait for the video to load
-    video.addEventListener("loadeddata", function() {
-      // Play the video after the source is changed and it's loaded
-      video.play();
-    });
   }
 
   // Add click event listener to the click text
