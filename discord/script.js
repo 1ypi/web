f8f814fac9dee30faf45c2f69f281f97a59a1419
1ypi <137641825+1ypi@users.noreply.github.com>
2023-07-25 17:43:49 +0200

    Update script.js

--- discord/script.js
+++ discord/script.js
@@ -1,10 +1,22 @@
+let cooldownActive = false;
+
 document.getElementById("message-form").addEventListener("submit", (event) => {
   event.preventDefault();
+
+  if (cooldownActive) {
+    alert("Por favor, espera 5 minutos antes de enviar otro mensaje.");
+    return;
+  }
 
   const name = document.getElementById("name").value;
   const message = document.getElementById("message").value;
 
   if (name && message) {
+    if (containsLink(message)) {
+      alert("No se permiten enlaces en el mensaje.");
+      return;
+    }
+
     const webhookURL = "https://discord.com/api/webhooks/1126202607132745871/O-DIXoAPbM_MqPwuHhUmQUUMIS5JD6V1rjZ-J70dWBjj3hsU4xkkXMWOQRVNqwkulAMx";
 
     const payload = {
@@ -18,12 +30,29 @@ document.getElementById("message-form").addEventListener("submit", (event) => {
       },
       body: JSON.stringify(payload),
     })
-      .then(() => alert("Mensaje enviado con éxito"))
+      .then(() => {
+        alert("Mensaje enviado con éxito");
+        document.getElementById("name").value = "";
+        document.getElementById("message").value = "";
+        setCooldown();
+      })
       .catch((error) => alert("Error al enviar el mensaje: " + error));
-
-    document.getElementById("name").value = "";
-    document.getElementById("message").value = "";
   } else {
     alert("Por favor, ingresa tu nombre y un mensaje antes de enviar");
   }
 });
+
+function setCooldown() {
+  cooldownActive = true;
+  document.getElementById("send-button").setAttribute("disabled", "true");
+
+  setTimeout(() => {
+    cooldownActive = false;
+    document.getElementById("send-button").removeAttribute("disabled");
+  }, 300000); // 5 minutos en milisegundos
+}
+
+function containsLink(text) {
+  const linkRegex = /(https?:\/\/[^\s]+)/g;
+  return linkRegex.test(text);
+}
