15a1493dbd77f1c171a82af41878d5f2dd822669
1ypi <137641825+1ypi@users.noreply.github.com>
2023-09-30 11:34:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -74,12 +74,12 @@ document.addEventListener("DOMContentLoaded", function() {
                 }
             }
 
-            title.innerHTML = `<a href="https://wanted.lol/1ypi" target="_blank" class="gradient-text">${hackedText}</a>`;
+            title.innerHTML = `<a href="https://www.youtube.com/channel/UC3tDacQMJjuzfTJUInF2MFw" target="_blank" class="gradient-text">${hackedText}</a>`;
 
             if (iteration >= originalText.length) {
                 clearInterval(interval);
                 setTimeout(() => {
-                    title.innerHTML = `<a href="https://wanted.lol/1ypi" target="_blank" class="gradient-text">${originalText}</a>`;
+                    title.innerHTML = `<a href="https://www.youtube.com/channel/UC3tDacQMJjuzfTJUInF2MFw" target="_blank" class="gradient-text">${originalText}</a>`;
                 }, 2000);
             }
 
