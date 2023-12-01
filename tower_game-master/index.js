61bccb23067d25b9f815188a857423a83aeeb9fc
1ypi <137641825+1ypi@users.noreply.github.com>
2023-12-01 22:21:03 +0100

    Update index.js

--- tower_game-master/index.js
+++ tower_game-master/index.js
@@ -4,11 +4,11 @@ const opn = require('opn')
 
 const server = express()
 const host = 'http://localhost:8082'
-server.use('./tower_game-master/assets', express.static(path.resolve(__dirname, './assets')))
-server.use('./tower_game-master/dist', express.static(path.resolve(__dirname, './dist')))
+server.use('/assets', express.static(path.resolve(__dirname, './tower_game-master/assets')))
+server.use('/dist', express.static(path.resolve(__dirname, './tower_game-master/dist')))
 
 server.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './tower_game-master/index.html'));
+  res.sendFile(path.resolve(__dirname, './index.html'));
 })
 
 server.listen(8082, () => {
