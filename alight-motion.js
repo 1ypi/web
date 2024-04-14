37ea5ff0487418c86f34545157a0bfb5ce60b576
1ypi <137641825+1ypi@users.noreply.github.com>
2024-04-14 14:02:16 +0200

    Update alight-motion.js

--- alight-motion.js
+++ alight-motion.js
@@ -1,8 +1,8 @@
-var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
-if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+var _____WB$wombat$assign$function_____ = function(name) {return (window._wb_wombat && window._wb_wombat.local_init && window._wb_wombat.local_init(name)) || window[name]; };
+if (!window.__WB_pmw) { window.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
 {
   let window = _____WB$wombat$assign$function_____("window");
-  let self = _____WB$wombat$assign$function_____("self");
+  let self = _____WB$wombat$assign$function_____("self"); // Cambiar esta línea a let window = _____WB$wombat$assign$function_____("window");
   let document = _____WB$wombat$assign$function_____("document");
   let location = _____WB$wombat$assign$function_____("location");
   let top = _____WB$wombat$assign$function_____("top");
@@ -47,29 +47,5 @@ obj= {
   }
 }
 
-
-
 $done({body: JSON.stringify(obj)});
-
 }
-/*
-     FILE ARCHIVED ON 19:04:32 May 30, 2023 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 11:32:23 Apr 14, 2024.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
-/*
-playback timings (ms):
-  captures_list: 0.606
-  exclusion.robots: 0.064
-  exclusion.robots.policy: 0.057
-  cdx.remote: 0.053
-  esindex: 0.007
-  LoadShardBlock: 54.536 (3)
-  PetaboxLoader3.datanode: 242.597 (5)
-  load_resource: 411.949
-  PetaboxLoader3.resolve: 200.184
-  loaddict: 65.23
-*/
